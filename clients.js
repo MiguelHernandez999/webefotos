a481702a62ddde1f52a6735340aee6e91479a16b
MiguelHernandez999 <nestor1999lopez@gmail.com>
2025-07-21 05:08:02 -0700

    beta

---
+++ clients.js
@@ -0,0 +1,93 @@
+// clients.js - Datos de los graduados
+const clients = {
+    "ailyn": {
+        name: "Ailyn Villavicencio García",
+        photo: "URL_FOTO_AILYN",
+        driveLink: "https://drive.google.com/drive/folders/1GLFiVE4h6UH9b-1levQz2njAz6f1RVOW?usp=sharing",
+        password: "AilVil789"
+    },
+    "emily": {
+        name: "Emily Benson Aguilar",
+        photo: "emily.jpg",
+        driveLink: "https://drive.google.com/drive/folders/1M31Yryg9hTFOs_iQ2tjGQo615O0X-Xlf?usp=drive_link",
+        password: "EmiBen456"
+    },
+    "genesis": {
+        name: "Genesis Amador Villavicencio",
+        photo: "URL_FOTO_GENESIS",
+        driveLink: "https://drive.google.com/drive/folders/15R8FMH_SIFhjnt_X35OtzwSDnbcoj5sc?usp=sharing",
+        password: "GenAma123"
+    },
+    "irlanda": {
+        name: "Irlanda Hernández Espinoza",
+        photo: "URL_FOTO_IRLANDA",
+        driveLink: "https://drive.google.com/drive/folders/1rjHSBrvo4RKrdhWO5gyunstQIsNlI6Q9?usp=sharing",
+        password: "IrlHer789"
+    },
+    "jareth": {
+        name: "Jareth Romo Covarrubias",
+        photo: "URL_FOTO_JARETH",
+        driveLink: "https://drive.google.com/drive/folders/1jjVxM0QqxXLZyO_kPQSjFX_kyM5PE2oF?usp=sharing",
+        password: "JarRom456"
+    },
+    "julio": {
+        name: "Julio González Villavicencio",
+        photo: "URL_FOTO_JULIO",
+        driveLink: "https://drive.google.com/drive/folders/1JGzn8gGKyHm0efvHLcLgoHKJo92eLPtE?usp=sharing",
+        password: "JulGon123"
+    },
+    "mariana": {
+        name: "Mariana Fuerte Rojas",
+        photo: "URL_FOTO_MARIANA",
+        driveLink: "https://drive.google.com/drive/folders/1GowNl8MmQBy0PR3Qu2QGaKqwdvR4gzd3?usp=sharing",
+        password: "MarFue789"
+    },
+    "marina": {
+        name: "Marina Rosales Aguilar",
+        photo: "URL_FOTO_MARINA",
+        driveLink: "https://drive.google.com/drive/folders/1blvl_ePbNDvfI1CgpOtYOGV9I9qADvq3?usp=sharing",
+        password: "MarRos456"
+    },
+    "nereida": {
+        name: "Nereida Moreno Leal",
+        photo: "URL_FOTO_NEREIDA",
+        driveLink: "https://drive.google.com/drive/folders/1c63kwn2TR_gag_-1EjsUzPQ5jzQEAjnN?usp=sharing",
+        password: "NerMor123"
+    },
+    "paul": {
+        name: "Paul Arce Fernandez",
+        photo: "URL_FOTO_PAUL",
+        driveLink: "https://drive.google.com/drive/folders/12eaV4ZAh9O8jqNHQV_1F4paJbdfZHQz9?usp=sharing",
+        password: "PauArc789"
+    },
+    "roberto": {
+        name: "Roberto González Cañedo",
+        photo: "URL_FOTO_ROBERTO",
+        driveLink: "https://drive.google.com/drive/folders/1ccrR_4XGHRpQJesDM__M0LFe6cSDTZPn?usp=sharing",
+        password: "RobGon456"
+    },
+    "sergio": {
+        name: "Sergio Landeros Altamirano",
+        photo: "URL_FOTO_SERGIO",
+        driveLink: "https://drive.google.com/drive/folders/1H3Bti8UTK0bwEi_sg0ZV9QyQLTuJk_Gh?usp=sharing",
+        password: "SerLan123"
+    },
+    "sofia": {
+        name: "Sofía Palafox García",
+        photo: "URL_FOTO_SOFIA",
+        driveLink: "https://drive.google.com/drive/folders/1S6KcelJS-ceGcCXgLdjzrlxRUmFs0Gue?usp=sharing",
+        password: "SofPal789"
+    },
+    "valeria": {
+        name: "Valeria González Michel",
+        photo: "URL_FOTO_VALERIA",
+        driveLink: "https://drive.google.com/drive/folders/1FkSYJJ6k6q5MEfxSVFEJ6Hiox3-VCYkb?usp=sharing",
+        password: "ValGon456"
+    },
+    "wendy": {
+        name: "Wendy Arias Benítez",
+        photo: "URL_FOTO_WENDY",
+        driveLink: "https://drive.google.com/drive/folders/1yt8gsqtpKwyKsElsJaQbkDSCRk_KHqEv?usp=sharing",
+        password: "WenAri123"
+    }
+};
